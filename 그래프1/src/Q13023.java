ebeee5f94569dfe7fd641a03041c7f217fdeeb4c
KIMJUSUNG <dlrtls12345@naver.com>
2020-10-26 14:59:45 +0900

    Q13023

--- 그래프1/src/Q13023.java
+++ 그래프1/src/Q13023.java
@@ -1,17 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Q13023 {
 
     static int n;
     static int m;
-    static int startVertex;
-    static Stack<Integer> stack;
-    static int[][] A;
-    static boolean[] check;
+    static GraphList graph = null;
+    static boolean[] check = null;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -19,53 +18,77 @@ public class Q13023 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         n = Integer.parseInt(st.nextToken());   // 사람의 수
         m = Integer.parseInt(st.nextToken());   // 친구 관계의 수
-        stack = new Stack<>();
+        graph = new GraphList(n);
         check = new boolean[n];
 
-        // 인접행렬구성
-        A = new int[n][n];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int rowIndex = Integer.parseInt(st.nextToken());
-            int columnIndex = Integer.parseInt(st.nextToken());
-            A[rowIndex][columnIndex] = A[columnIndex][rowIndex] = 1;
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            graph.put(v1, v2);
+        }
 
-            // 처음 입력받는 노드를 시작노드로 지정
-            if (i == 0) {
-                startVertex = rowIndex;
+        // graph.printGraph();
+
+        for (int i = 0; i < n; i++) {
+
+            dfs(i, 0);
+
+            if (answer == 1) {
+                break;
             }
         }
 
-        // dfs 로 탐색
-        System.out.print(dfs());
+        System.out.print(answer);
     }
 
-    public static int dfs() {
-        stack.push(startVertex);
-        check[startVertex] = true;
+    public static void dfs(int x, int depth) {
 
-        while (!stack.isEmpty()) {
-            int top = stack.peek();
-            boolean isConnected = false;    // top 노드에서 탐색가능한 노드가 있는지
-            for (int i = 0; i < n; i++) {
-                if (A[top][i] == 1 && check[i] == false) {
-                    isConnected = true;
-                    check[i] = true;
-                    stack.push(i);
-                    if (stack.size() == 4) {
-                        for (int j = 0; j < n; j++) {
-                            if (A[stack.peek()][j] == 1) {
-                                return 1;
-                            }
-                        }
-                    }
-                    break;
-                }
-            }
-            if (isConnected == false) {
-                stack.pop();
+        if (depth == 4) {
+            answer = 1;
+            return;
+        }
+
+        check[x] = true;
+        for (int i = 0; i < graph.getList().get(x).size(); i++) {
+            int v = graph.getList().get(x).get(i);
+            if (check[v] == false) {
+                dfs(v, depth + 1);
             }
         }
-        return 0;
+        check[x] = false;
+    }
+}
+
+// 인접리스트 클래스
+class GraphList {
+
+    private ArrayList<ArrayList<Integer>> list;
+
+    public GraphList(int listSize) {
+        list = new ArrayList<>();
+        for (int i = 0; i < listSize; i++) {
+            list.add(new ArrayList<Integer>());
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> getList() {
+        return this.list;
+    }
+
+    // 양방향 그래프 추가
+    public void put(int x, int y) {
+        list.get(x).add(y);
+        list.get(y).add(x);
+    }
+
+    // 그래프 출력
+    public void printGraph() {
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print("정점 " + i + "의 인접리스트");
+            for(int j = 0; j < list.get(i).size(); j++) {
+                System.out.print(" -> " + list.get(i).get(j));
+            } System.out.println();
+        }
     }
 }
