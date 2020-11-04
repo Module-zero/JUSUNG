551057ff39fdb1a357e1dbed7f93d992cf30f023
KIMJUSUNG <dlrtls12345@naver.com>
2020-11-04 16:50:17 +0900

    Q16197

--- 브루트 포스/src/Q16197.java
+++ 브루트 포스/src/Q16197.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Q16197 {
@@ -9,6 +10,7 @@ public class Q16197 {
     static int N;
     static int M;
     static char[][] board;
+    static boolean[][] check;
     static ArrayList<Integer> answers = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
@@ -18,6 +20,8 @@ public class Q16197 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         board = new char[N][M];
+        check = new boolean[N][M];
+
         int x1 = -1;
         int y1 = -1;
         int x2 = -1;
@@ -42,69 +46,75 @@ public class Q16197 {
 
         go(x1, y1, x2, y2, 0, 0, 0);
 
-        for (Integer i : answers) {
-            System.out.print(i + " ");
+        Collections.sort(answers);
+        if (answers.size() == 0) {
+            System.out.print(-1);
+        }
+        else {
+            System.out.print(answers.get(0));
         }
     }
 
     static void go(int x1, int y1, int x2, int y2, int backX, int backY, int count) {
 
-        if (!check(x1, y1) && !check(x2, y2)) {
+        /*
+        System.out.print(x1 + "," + y1 + " / ");
+        System.out.println(x2 + "," + y2);
+         */
 
-            System.out.print(x1 + "," + y1 + " / ");
-            System.out.println(x2 + "," + y2);
+        if (count > 10) {
+            return;
+        }
 
-            if (board[x1][y1] == '#' && board[x2][y2] == '#') {
-                System.out.println("blocked");
-                return;
-            }
-
-            if (board[x1][y1] == '#') {
-                x1 += backX;
-                y1 += backY;
-            }
-
-            if (board[x2][y2] == '#') {
-                x2 += backX;
-                y2 += backY;
-            }
+        if ((x1 == x2) && (y1 == y2)) {
+            return;
         }
 
         if (check(x1, y1) && check(x2, y2)) {
-            System.out.println("둘 다 떨어짐");
+            // System.out.println("둘 다 떨어짐\n");
             return;
         }
 
         if (check(x1, y1) && !check(x2, y2)) {
+            // System.out.println("1번 동전만 떨어짐\n");
             answers.add(count);
             return;
         }
 
         if (!check(x1, y1) && check(x2, y2)) {
+            // System.out.println("2번 동전만 떨어짐\n");
             answers.add(count);
             return;
         }
 
+        if (board[x1][y1] == '#' && board[x2][y2] == '#') {
+            // System.out.println("둘 다 벽 만남\n");
+            return;
+        }
+
+        if (board[x1][y1] == '#' && board[x2][y2] != '#') {
+            // System.out.println("1번 동전만 벽 만남");
+            x1 += backX;
+            y1 += backY;
+        }
+
+        if (board[x1][y1] != '#' && board[x2][y2] == '#') {
+            // System.out.println("2번 동전만 벽 만남");
+            x2 += backX;
+            y2 += backY;
+        }
+
+        if (check[x1][y1] && check[x2][y2]) {
+            return;
+        }
+
         // 왔던 곳으로 되돌아가지 않도록 함
-        if (backX != -1) {
-            go(x1 - 1, y1, x2 - 1, y2, 1, 0, count + 1);
-        }
-
-        if (backX != 1) {
-            go(x1 + 1, y1, x2 + 1, y2, -1, 0, count + 1);
-        }
-
-        System.out.println("check");
-
-        if (backY != -1) {
-            go(x1, y1 - 1, x2, y2 - 1, 0, 1, count + 1);
-        }
-
-        if (backY != 1) {
-            go(x1, y1 + 1, x2, y2 + 1, 0, -1, count + 1);
-        }
-
-        return;
+        check[x1][y1] = check[x2][y2] = true;
+        go(x1 - 1, y1, x2 - 1, y2, 1, 0, count + 1);
+        go(x1 + 1, y1, x2 + 1, y2, -1, 0, count + 1);
+        go(x1, y1 - 1, x2, y2 - 1, 0, 1, count + 1);
+        go(x1, y1 + 1, x2, y2 + 1, 0, -1, count + 1);
+        check[x1][y1] = check[x2][y2] = false;
     }
 
     static boolean check(int x, int y) {
