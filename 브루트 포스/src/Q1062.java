9b738eef98423ced82939679a70f484e618f1b9d
KIMJUSUNG <dlrtls12345@naver.com>
2020-11-10 20:30:13 +0900

    Q1062

--- 브루트 포스/src/Q1062.java
+++ 브루트 포스/src/Q1062.java
@@ -3,6 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// K개의 글자를 가르칠 때, 학생들이 읽을 수 있는 단어 개수의 최댓값을 출력
+// -> '최대값' 을 구해야 하므로 '모든 경우' 를 돌며 최대값을 갱신해야함
+// -> 그렇다면 '모든 경우' 는 무엇인가?
+// -> 기본적으로 'a, c, i, n, t' 5개의 기본 글자는 반드시 포함되어야 단어를 읽을 수 있음.
+// -> 글자는 k 개가 주어지므로 k-5 개의 글자를 21 개의 글자에서 중복없이 뽑은 뒤, 5개의 기본 글자와 합치는 것이 모든 경우임
+
+// 재귀를 사용하여 dfs 로 모든 경우를 탐색
+
+// 재귀를 사용할 때는 종료조건의 설정이 아주 중요함
+// 나는 재귀가 끝나면 추출된 k-5 개의 글자를 기본문자와 어떻게 적절히 합칠지를 생각해내지 못함
+// 그냥 check 배열에 체크해두면 되는 것이였음
+// 결국 check 배열에 k 개의 글자를 모두 체크한 뒤 주어진 단어들을 체크함
+
 public class Q1062 {
 
     static int N;
@@ -22,11 +35,6 @@ public class Q1062 {
 
         if (K < 5) {
             System.out.print(0);
-            return;
-        }
-
-        if (K == 26) {
-            System.out.print(N);
             return;
         }
 
@@ -52,7 +60,7 @@ public class Q1062 {
 
     static void go(int depth, int start) {
 
-        // 사용가능한 알파벳이 check 배열에 체크되어있음
+        // K개의 사용가능한 알파벳이 check 배열에 체크되어있음
         if (depth == K-5) {
             int count = 0;
             for (int i = 0; i < words.length; i++) {
