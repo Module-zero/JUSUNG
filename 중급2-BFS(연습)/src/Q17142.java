afd4e62179a8343325dfbb71e9289455464ad055
KIMJUSUNG <dlrtls12345@naver.com>
2020-11-26 15:03:54 +0900

    Q17142

---
+++ 중급2-BFS(연습)/src/Q17142.java
@@ -0,0 +1,121 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Q17142 {
+
+    static int n, m;
+    static int[][] map;
+    static int[] order;
+    static ArrayList<Pair> virusPair = new ArrayList<>();
+    static int answer = Integer.MAX_VALUE;
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+        order = new int[m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    virusPair.add(new Pair(i, j));
+                }
+            }
+        }
+
+        go(0, 0);
+
+        if (answer == Integer.MAX_VALUE) {
+            System.out.print(-1);
+        }
+        else {
+            System.out.print(answer);
+        }
+
+    }
+
+    static void go(int index, int start) {
+
+        if (index == m) {
+            LinkedList<Pair> q = new LinkedList<>();
+            int[][] visited = new int[n][n];
+            int max = Integer.MIN_VALUE;
+            for (int i : order) {
+                q.add(virusPair.get(i));
+                visited[virusPair.get(i).getX()][virusPair.get(i).getY()] = 1;
+            }
+
+            while (!q.isEmpty()) {
+
+                Pair p = q.poll();
+
+                if (map[p.getX()][p.getY()] == 0 || visited[p.getX()][p.getY()] == 1) {
+                    if (max < visited[p.getX()][p.getY()]) {
+                        max = visited[p.getX()][p.getY()];
+                    }
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = p.getX() + dx[i];
+                    int ny = p.getY() + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                        continue;
+                    }
+                    if (map[nx][ny] != 1 && visited[nx][ny] == 0) {
+                        visited[nx][ny] = visited[p.getX()][p.getY()] + 1;
+                        q.add(new Pair(nx, ny));
+                    }
+                }
+            }
+
+            // 모든 빈칸이 다 찼을 경우에만 정답 갱신
+            if (isFull(visited)) {
+                if (answer > max - 1) {
+                    answer = max - 1;
+                }
+            }
+
+            return;
+        }
+
+        for (int i = start; i < virusPair.size(); i++) {
+            order[index] = i;
+            go(index+1, i+1);
+        }
+    }
+
+    static boolean isFull(int[][] visited) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 0 && visited[i][j] == 0) {
+                    return false;
+                } else if (map[i][j] == 2 && visited[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static class Pair {
+        private int x;
+        private int y;
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX() { return x; }
+        public int getY() { return y; }
+    }
+
+}
--- 중급2-BFS(연습)/src/Q2151.java
+++ 중급2-BFS(연습)/src/Q2151.java
@@ -2,17 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 
+// 최소로 거울을 사용하여 반대편 문을 바라볼 수 있도록 하는 문제
+// 이 문제의 핵심은 '방향' 임. 그리고 우선순위 큐를 사용하는 것임
+// 좌표상에서 어떤 '최소값' 을 찾아내는 문제는 우선순위 큐로 해결가능함
+// 대부분은 cost 가 작은것이 먼저 나오게한 뒤, 해당 좌표를 방문처리해버리면
+// 다음에 같은 좌표지만 cost 가 더 큰것이 와도 값을 갱신할 수 없음
 public class Q2151 {
 
     static int N;
     static char[][] map;
     static int[][][] visited;
     static ArrayList<Pair> twoDoors;
-    static LinkedList<Pair> queue;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
 
     public static void main(String[] args) throws IOException {
 
@@ -21,7 +25,6 @@ public class Q2151 {
         map = new char[N][N];
         visited = new int[N][N][4];
         twoDoors = new ArrayList<>();
-        queue = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
@@ -29,7 +32,7 @@ public class Q2151 {
                 map[i][j] = s.charAt(j);
                 if (map[i][j] == '#') {
                     // 양쪽 문의 좌표를 저장
-                    twoDoors.add(new Pair(i, j, -1));
+                    twoDoors.add(new Pair(i, j, -1, 0));
                 }
             }
         }
@@ -37,38 +40,87 @@ public class Q2151 {
         bfs();
     }
 
-    // 0 : 아래
-    // 1 : 위
-    // 2 : 오른쪽
-    // 3 : 왼쪽
+    // 0 : 서
+    // 1 : 북
+    // 2 : 동
+    // 3 : 남
     static void bfs() {
 
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+        Pair end = twoDoors.get(1);
+
+        // 시작위치를 모든 방향으로 세팅해서 모두 큐에 넣음
         for (int i = 0; i < 4; i++) {
-            twoDoors.get(0).setDir(i);
-            queue.add(twoDoors.get(0));
+            Pair start = new Pair(twoDoors.get(0).getX(), twoDoors.get(0).getY(), i, 0);
+            q.add(start);
         }
 
-        while (!queue.isEmpty()) {
-            Pair p = queue.poll();
+        while (!q.isEmpty()) {
 
-            if (map[p.getX()][p.getY()] == '!') {
+            Pair p = q.poll();
 
+            // 우선 순위 큐를 사용하기 때문에 항상 최소의 cost 를 가지고 있는 좌표가 먼저 나옴
+            // 즉 가장 먼저 end 좌표를 찾는 경우가 가장 최소의 cost 를 가지고 있는 경우임
+            if ((p.getX() == end.getX()) && (p.getY() == end.getY())) {
+               System.out.print(p.getCost());
+               break;
             }
+
+            // 좌표를 우선순위 큐에 넣었기 때문에
+            // cost(사용한 거울의 개수)가 작은 좌표가 먼저 나옴
+            // 최소값을 구하는 문제이므로 먼저 나온 좌표를 먼저 방문으로 체크해야
+            // 나중에 cost 가 더 큰 좌표가 방문하지 않게 할 수 있음
+            if (visited[p.getX()][p.getY()][p.getDir()] == 1) {
+                continue;
+            }
+            visited[p.getX()][p.getY()][p.getDir()] = 1;
+
+            // for 문으로 서북동남 전 방향으로 이동하는 것이 아님
+            // 기본적으로는 부모의 방향을 따라서 이동해야함
+            int nx = p.getX() + dx[p.getDir()];
+            int ny = p.getY() + dy[p.getDir()];
+
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+                continue;
+            }
+            if (map[nx][ny] == '*') {
+                continue;
+            }
+
+            // 거울을 사용한 경우 진행방향의 양쪽방향도 q 에 넣어야함
+            // 거울에 반사된 경우이므로 부모 cost+1 을 함
+            if (map[nx][ny] == '!') {
+                q.add(new Pair(nx, ny, (p.getDir()+1)%4, p.getCost()+1));
+                q.add(new Pair(nx, ny, (p.getDir()+3)%4, p.getCost()+1));
+            }
+
+            // 거울을 사용하지 않을 경우 부모의 cost 를 그대로 가져감
+            q.add(new Pair(nx, ny, p.getDir(), p.getCost()));
         }
     }
 
-    static class Pair {
+    static class Pair implements Comparable<Pair> {
         private int x;
         private int y;
         private int dir;
-        public Pair(int x, int y, int dir) {
+        private int cost;
+
+        public Pair(int x, int y, int dir, int cost) {
             this.x = x;
             this.y = y;
             this.dir = dir;
+            this.cost = cost;
         }
+
         public int getX() { return x; }
         public int getY() { return y; }
         public int getDir() { return dir; }
-        public void setDir(int dir) { this.dir = dir; }
+        public int getCost() { return cost; }
+
+        @Override
+        public int compareTo(Pair o) {
+            // q 에서 cost 가 작은 객체 먼저 나옴
+            return this.cost - o.cost;
+        }
     }
 }
