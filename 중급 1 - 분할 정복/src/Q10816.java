9b2b1fb0ca37d44f0f2f3294e6e4dbac734457a4
KIMJUSUNG <dlrtls12345@naver.com>
2020-12-14 15:06:52 +0900

    Q10816 not solved

--- 중급 1 - 분할 정복/src/Q10816.java
+++ 중급 1 - 분할 정복/src/Q10816.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Q10816 {
     static int n, m;
     static int[] arr1, arr2;
+    static HashMap<Integer, Integer> map = new HashMap<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,6 +16,12 @@ public class Q10816 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             arr1[i] = Integer.parseInt(st.nextToken());
+            if (!map.containsKey(arr1[i])) {
+                map.put(arr1[i], 1);
+            } else {
+                int value = map.get(arr1[i]);
+                map.put(arr1[i], value + 1);
+            }
         }
         m = Integer.parseInt(br.readLine());
         arr2 = new int[m];
@@ -24,31 +31,26 @@ public class Q10816 {
         }
 
         Arrays.sort(arr1);
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            if (binarySearch(arr2[i])) {
-                sb.append(1+" ");
-            } else {
-                sb.append(0+" ");
-            }
+            binarySearch(arr2[i]);
         }
         System.out.print(sb);
     }
 
     // 이분탐색
-    static boolean binarySearch(int x) {
+    static void binarySearch(int x) {
         int start = 0;
         int end = arr1.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (arr1[mid] == x) {
-                return true;
+                sb.append(map.get(x)).append(" ");
             } else if (arr1[mid] < x) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        return false;
+        sb.append(0).append(" ");
     }
 }
