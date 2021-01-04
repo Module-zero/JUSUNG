44ce2bbb4c975cf9dd95a4bb03cc72732b062cd7
unknown <dlrtls12345@naver.com>
2021-01-05 01:44:51 +0900

    Q15684

--- 중급 2 - 브루트 포스/src/Q15684.java
+++ 중급 2 - 브루트 포스/src/Q15684.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Q15684 {
     static int n, m, h;
-    static int[][] map = new int[11][31];
+    static int[][] ladder = new int[100][100];
+    static ArrayList<int[]> candidate = new ArrayList<>();
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,31 +20,81 @@ public class Q15684 {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            map[a][b] = b+1;
-            map[a][b+1] = b;
+            ladder[a][b] = b+1;
+            ladder[a][b+1] = b;
         }
 
-        go(0, 0, new Pair(1, 1));
-    }
-
-    static void go(int index, int cnt, Pair np) {
-    }
-
-    static class Pair {
-        final private int x, y;
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+        for (int i = 1; i <= h; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j != n && ladder[i][j] == 0 && ladder[i][j+1] == 0) {
+                    // 사다리를 놓을 수 있는 후보 좌표들
+                    candidate.add(new int[]{i, j});
+                }
+            }
         }
-        public int getX() { return x; }
-        public int getY() { return y; }
+
+        // 사다리를 0개 추가하는 경우
+        if (checkResult()) {
+            System.out.print(0);
+            return;
+        }
+
+        // 사다리를 i 개 추가하는 경우
+        for (int i = 1; i <= 3; i++) {
+            visited = new boolean[9*30+1];
+
+            if (go(0, 0, i)) {
+                System.out.print(i);
+                return;
+            }
+        }
+
+        // 답이 나오지 않는 경우
+        System.out.print(-1);
     }
 
-    static void printMap() {
+    static boolean go(int index, int start, int ladderCount) {
+
+        if (index == ladderCount) {
+            return checkResult();
+        }
+
+        for (int i = start; i < candidate.size(); i++) {
+            if (!visited[i]) {
+                int[] p = candidate.get(i);
+                int x = p[0], y = p[1];
+                if (ladder[x][y] != 0 || ladder[x][y+1] != 0) {
+                    continue;
+                }
+
+                visited[i] = true;
+                ladder[x][y] = y+1;
+                ladder[x][y+1] = y;
+                if (go(index+1, i+1, ladderCount)) {
+                    return true;
+                }
+                ladder[x][y] = 0;
+                ladder[x][y+1] = 0;
+                visited[i] = false;
+            }
+        }
+        return false;
+    }
+
+    // i 번 세로선의 결과가 i 번이 나오는지를 체크
+    static boolean checkResult() {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= h; j++) {
-                System.out.print(map[i][j]+" ");
-            } System.out.println();
-        } System.out.println();
+            int prevCol = i, nextCol = 0;
+            for (int j = 1; j <= h + 1; j++) {
+                nextCol = ladder[j][prevCol];
+                if (nextCol != 0) {
+                    prevCol = nextCol;
+                }
+            }
+            if (i != prevCol) {
+                return false;
+            }
+        }
+        return true;
     }
 }
