28ba0dd95ca720c3859f4163c19cddabd6245f65
unknown <dlrtls12345@naver.com>
2021-01-06 01:04:52 +0900

    Q1644 not solved

---
+++ 중급 2 - 브루트 포스/src/Q1644.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1644 {
+    static int n;
+    static boolean[] check = new boolean[4000001];
+    static long[] dp = new long[4000001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        prime(n);
+
+        int idx = 1;
+        for (int i = 2; i <= n; i++) {
+            if (!check[i]) {
+                // System.out.print(i+" ");
+                dp[idx] = dp[idx-1] + i;
+                idx++;
+            }
+        }
+
+        int i = 1;
+        while (dp[i] != 0) {
+            System.out.print(dp[i] + " ");
+            i++;
+        }
+
+        /*
+        for (int i = 0; i <= n; i++) {
+            System.out.print(dp[i]+ " ");
+        }
+         */
+    }
+
+    static void prime(int x) {
+        for (int i = 2; i <= x; i++) {
+            if (!check[i]) {
+                for (int j = i*2; j <= x; j+=i) {
+                    check[j] = true;
+                }
+            }
+        }
+    }
+
+}
