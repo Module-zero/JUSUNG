afa39bd40d92f643e76c2a290660e43e2a6fb3de
KIMJUSUNG <dlrtls12345@naver.com>
2021-01-19 13:15:45 +0900

    Q6549

--- 중급 2 - 자료구조/src/Q6549.java
+++ 중급 2 - 자료구조/src/Q6549.java
@@ -1,27 +1,84 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Q6549 {
-    static int n;
-    static int[] rec, arr1, arr2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
             if (n == 0) {
                 break;
             }
-            rec = new int[n];
+
+            Stack<Pair> stack = new Stack<>();
+            int[] rec = new int[n];
+            int[] left = new int[n];
+            int[] right = new int[n];
+
             for (int i = 0; i < n; i++) {
                 rec[i] = Integer.parseInt(st.nextToken());
             }
-            arr1 = new int[n];
-            arr2 = new int[n];
 
+            for (int i = 0; i < n; i++) {
+                boolean flag = false;
+                while (!stack.isEmpty()) {
+                    Pair top = stack.peek();
+                    if (top.h >= rec[i]) {
+                        Pair tmp = stack.pop();
+                        left[i] = left[tmp.x];
+                        flag = true;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (!flag) {
+                    left[i] = i;
+                }
+                stack.push(new Pair(i, rec[i]));
+            }
+
+            stack.clear();
+
+            for (int i = n-1; i >= 0; i--) {
+                boolean flag = false;
+                while (!stack.isEmpty()) {
+                    Pair top = stack.peek();
+                    if (top.h >= rec[i]) {
+                        Pair tmp = stack.pop();
+                        right[i] = right[tmp.x];
+                        flag = true;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (!flag) {
+                    right[i] = i;
+                }
+                stack.push(new Pair(i, rec[i]));
+            }
+
+            long max = Long.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                int x = right[i] - left[i] + 1;
+                max = Math.max(max, (long)x * (long)rec[i]);
+            }
+
+            System.out.println(max);
+        }
+    }
+
+    static class Pair {
+        private final int x, h;
+        public Pair(int x, int h) {
+            this.x = x;
+            this.h = h;
         }
     }
 }
