7c32d0e6830243e6056a8999c8aaf0b1ca6c7713
KIMJUSUNG <dlrtls12345@naver.com>
2021-01-21 17:31:03 +0900

    Q3111

--- 중급 2 - 자료구조/src/Q3111.java
+++ 중급 2 - 자료구조/src/Q3111.java
@@ -7,7 +7,8 @@ public class Q3111 {
     static int n, m;
     static String A, T;
     static boolean[] used = new boolean[300001];
-    static Stack<int[]> stack = new Stack<>();
+    static Stack<int[]> stack1 = new Stack<>();
+    static Stack<int[]> stack2 = new Stack<>();
     static StringBuilder answer = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -17,7 +18,14 @@ public class Q3111 {
         n = T.length();
         m = A.length();
 
-        left();
+        while (true) {
+            if (!left()) {
+                break;
+            }
+            if (!right()) {
+                break;
+            }
+        }
 
         for (int i = 0; i < n; i++) {
             if (!used[i]) {
@@ -28,37 +36,88 @@ public class Q3111 {
     }
 
     static boolean left() {
-        for (int i = 0; i < n; i++) {
+        int start = 0;
+        if (!stack1.isEmpty()) {
+            start = stack1.peek()[0] + 1;
+        }
+
+        for (int i = start; i < n; i++) {
             if (used[i]) {
                 continue;
             }
             char cur = T.charAt(i);
-            if (cur == A.charAt(0)) {
-                stack.push(new int[]{i, 0});
+            if (stack1.isEmpty()) {
+                if (cur == A.charAt(0)) {
+                    stack1.push(new int[]{i, 0});
+                }
             } else {
-                if (!stack.isEmpty()) {
-                    int nIdx = stack.peek()[1] + 1;
-                    if (cur == A.charAt(nIdx)) {
-                        System.out.println(nIdx);
-                        stack.push(new int[]{i, nIdx});
-                        if (nIdx == m - 1) {
-                            System.out.println("c");
-                            for (int j = 0; j < m; j++) {
-                                int[] pair = stack.pop();
-                                int idx = pair[0];
-                                used[idx] = true;
-                            }
-                            while (!stack.isEmpty()) {
-                                stack.pop();
-                            }
-                            // A를 찾음
-                            return true;
+                int[] p = stack1.peek();
+                int nIdx = p[1] + 1;
+                if (cur == A.charAt(nIdx)) {
+                    stack1.push(new int[]{i, nIdx});
+                    if (nIdx == m - 1) {
+                        for (int j = 0; j < m; j++) {
+                            p = stack1.pop();
+                            used[p[0]] = true;
                         }
+                        // A 를 찾아서 삭제
+                        return true;
+                    }
+                } else {
+                    while (!stack1.isEmpty()) {
+                        stack1.pop();
+                    }
+                    if (cur == A.charAt(0)) {
+                        stack1.push(new int[]{i, 0});
                     }
                 }
             }
         }
-        // A를 못찾음
+        // A 가 없음
+        return false;
+    }
+
+    static boolean right() {
+        int start = n-1;
+        if (!stack2.isEmpty()) {
+            start = stack2.peek()[0] - 1;
+        }
+
+        for (int i = start; i >= 0; i--) {
+            if (used[i]) {
+                continue;
+            }
+            char cur = T.charAt(i);
+            if (stack2.isEmpty()) {
+                if (cur == A.charAt(m-1)) {
+                    System.out.println("c");
+                    stack2.push(new int[]{i, m-1});
+                }
+            } else {
+                int[] p = stack2.peek();
+                int nIdx = p[1] - 1;
+                if (cur == A.charAt(nIdx)) {
+                    System.out.println("c1");
+                    stack2.push(new int[]{i, nIdx});
+                    if (nIdx == 0) {
+                        for (int j = 0; j < m; j++) {
+                            p = stack2.pop();
+                            used[p[0]] = true;
+                        }
+                        // A 를 찾아서 삭제
+                        return true;
+                    }
+                } else {
+                    while (!stack2.isEmpty()) {
+                        stack2.pop();
+                    }
+                    if (cur == A.charAt(m-1)) {
+                        stack2.push(new int[]{i, m-1});
+                    }
+                }
+            }
+        }
+        // A 가 없음
         return false;
     }
 }
