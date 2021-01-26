c6ec51109d012805331645083c8c53b4a9895789
unknown <dlrtls12345@naver.com>
2021-01-27 00:04:50 +0900

    Q10942

--- 중급 2 - 다이나믹 프로그래밍/src/Q10942.java
+++ 중급 2 - 다이나믹 프로그래밍/src/Q10942.java
@@ -5,21 +5,16 @@ import java.util.StringTokenizer;
 
 public class Q10942 {
     static int n, m;
-    static int[] arr = new int[2021];
+    static int[] a = new int[2021];
     static int[][] d = new int[2021][2021];
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < 2021; i++) {
-            for (int j = 0; j < 2021; j++) {
-                d[i][j] = i == j ? 1 : -1;
-            }
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
         m = Integer.parseInt(br.readLine());
@@ -27,23 +22,33 @@ public class Q10942 {
             st = new StringTokenizer(br.readLine()," ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            System.out.println(go(x, y));
+            int k = (go(x, y) == 1 ? 1 : 0);
+            sb.append(k).append("\n");
         }
+
+        System.out.print(sb);
     }
 
+    // 0 : 방문 안함
+    // -1 : 방문 했는데 팰린드롬 아님
+    // 1 : 방문 했는데 팰린드롬 맞음
     static int go(int x, int y) {
 
-        if (d[x][y] != -1) {
+        if (x == y) {
+           return d[x][y] = 1;
+        }
+
+        if (d[x][y] != 0) {
             return d[x][y];
         }
 
         if (x + 1 == y) {
-            return d[x][y] = (arr[x] == arr[y] ? 1 : 0);
+            return d[x][y] = (a[x] == a[y] ? 1 : -1);
         }
 
-        d[x][y] = 0;
+        d[x][y] = -1;
         if (go(x+1, y-1) == 1) {
-            if (arr[x] == arr[y]) {
+            if (a[x] == a[y]) {
                 d[x][y] = 1;
             }
         }
