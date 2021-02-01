20e5284397ac03a1e48973da8dba9e0f2d6942bc
unknown <dlrtls12345@naver.com>
2021-02-02 06:14:16 +0900

    Q11066

--- 중급 2 - 다이나믹 프로그래밍/src/Q11066.java
+++ 중급 2 - 다이나믹 프로그래밍/src/Q11066.java
@@ -6,7 +6,8 @@ import java.util.StringTokenizer;
 public class Q11066 {
     static int t;
     static int[] arr;
-    static long[][] d1, d2;
+    static long[][] d;
+    static long[] sum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,28 +15,28 @@ public class Q11066 {
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             arr = new int[n];
-            d1 = new long[n][n];
-            d2 = new long[n][n];
+            d = new long[n][n];
+            sum = new long[n];
 
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            long x = 0;
             for (int i = 0; i < n; i++) {
-                d1[i][i] = arr[i] = Integer.parseInt(st.nextToken());
+                arr[i] = Integer.parseInt(st.nextToken());
+                sum[i] = (x += arr[i]);
             }
 
             for (int i = 2; i <= n; i++) {
                 for (int j = 0; j <= n-i; j++) {
-                    long min1 = Long.MAX_VALUE;
-                    long min2 = Long.MAX_VALUE;
+                    long min = Long.MAX_VALUE;
                     for (int k = j; k <= j+i-2; k++) {
-                        min1 = Math.min(min1, d1[j][k] + d1[k+1][j+i-1]);
-                        min2 = Math.min(min2, d2[j][k] + d2[k+1][j+i-1]);
+                        min = Math.min(min, d[j][k] + d[k+1][j+i-1]);
                     }
-                    d1[j][j+i-1] = min1;
-                    d2[j][j+i-1] = min1 + min2;
+                    long s = (j == 0 ? sum[j+i-1] : sum[j+i-1] - sum[j-1]);
+                    d[j][j+i-1] = min + s;
                 }
             }
 
-            System.out.println(d2[0][n-1]);
+            System.out.println(d[0][n-1]);
         }
     }
 }
