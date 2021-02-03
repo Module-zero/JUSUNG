997b02cfb1ed3556aabe0192ad07e394c0b3e139
KIMJUSUNG <dlrtls12345@naver.com>
2021-02-03 17:11:32 +0900

    Q12865

--- 중급 2 - 다이나믹 프로그래밍/src/Q12865.java
+++ 중급 2 - 다이나믹 프로그래밍/src/Q12865.java
@@ -21,6 +21,14 @@ public class Q12865 {
         }
 
         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                d[i][j] = d[i-1][j];
+                if (j >= w[i]) {
+                    d[i][j] = Math.max(d[i-1][j-w[i]] + v[i], d[i-1][j]);
+                }
+            }
         }
+
+        System.out.print(d[n][k]);
     }
 }
