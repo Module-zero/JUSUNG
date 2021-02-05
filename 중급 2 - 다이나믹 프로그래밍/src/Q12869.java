6126e24fb852284d9aac485b8703ee0f59c3f8ff
KIMJUSUNG <dlrtls12345@naver.com>
2021-02-05 15:21:07 +0900

    Q12869

--- 중급 2 - 다이나믹 프로그래밍/src/Q12869.java
+++ 중급 2 - 다이나믹 프로그래밍/src/Q12869.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Q12869 {
     static int n;
     static int min = Integer.MAX_VALUE;
-    static int[] life = new int[4];
+    static int[] l = new int[3];
     static int[][][] d = new int[61][61][61];
     static int[][] a = {{1, 3, 9}, {1, 9, 3}, {3, 1, 9}, {3, 9, 1}, {9, 1, 3}, {9, 3, 1}};
 
@@ -14,8 +14,8 @@ public class Q12869 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= n; i++) {
-            life[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            l[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < 61; i++) {
@@ -26,20 +26,15 @@ public class Q12869 {
             }
         }
 
-        go(life[1], life[2], life[3]);
+        d[l[0]][l[1]][l[2]] = 0;
+        go(l[0], l[1], l[2]);
         System.out.print(min);
     }
 
     static void go(int x, int y, int z) {
 
-        System.out.println(x+","+y+","+z);
-
         if (x == 0 && y == 0 && z == 0) {
             min = Math.min(min, d[x][y][z]);
-            return;
-        }
-
-        if (d[x][y][z] != -1) {
             return;
         }
 
@@ -47,12 +42,6 @@ public class Q12869 {
             int nx = Math.max(x - a[i][0], 0);
             int ny = Math.max(y - a[i][1], 0);
             int nz = Math.max(z - a[i][2], 0);
-
-            if (d[nx][ny][nz] == -1) {
-                d[nx][ny][nz] = d[x][y][z] + 1;
-            } else {
-                d[nx][ny][nz] = Math.min(d[nx][ny][nz], d[x][y][z] + 1);
-            }
 
             go(nx, ny, nz);
         }
